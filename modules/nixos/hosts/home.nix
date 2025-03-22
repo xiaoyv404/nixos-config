4ed28e4a6fafb032199aa50748e5cf356b979819
xiaoyv404 <xiaoyv_404@outlook.com>
2025-03-22 17:07:55 +0800

    fix mini configration
    Co-authored-by: Kris Hu <krishukr@users.noreply.github.com>

--- modules/nixos/hosts/home.nix
+++ modules/nixos/hosts/home.nix
@@ -17,7 +17,7 @@
     gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 1w";
+      options = "--delete-older-than 14d";
     };
     registry.nixpkgs.flake = inputs.nixpkgs;
     channel.enable = false;
