5dd7c87b98fe80a3afb8ab1fb9e5fb7466617bbc
xiaoyv_404 <xiaoyv_404@outlook.com>
2025-03-30 14:33:50 +0800

    fix home-manager's version mistake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/24.11";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     vscode-server.url = "github:nix-community/nixos-vscode-server";
