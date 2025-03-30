445104ea708a27b2c8920c0ba4dd16e24ad1fde9
xiaoyv_404 <xiaoyv_404@outlook.com>
2025-03-30 14:15:51 +0800

    seperate pkgs in home-manager from nixos configuration to enable config to nixpkgs.*

--- hosts/nixos-vm/default.nix
+++ hosts/nixos-vm/default.nix
@@ -17,7 +17,7 @@
 
     inputs.home-manager.nixosModules.home-manager
     {
-      home-manager.useGlobalPkgs = true;
+      # home-manager.useGlobalPkgs = true;
       home-manager.useUserPackages = true;
       home-manager.extraSpecialArgs = {
         inherit inputs outputs;
